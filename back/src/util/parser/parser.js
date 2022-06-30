c357731a0bb635a5cb755ce17a5903016925ba31
Hyejin Eom <hjeom.dev@gmail.com>
2022-07-01 00:40:16 +0900

    add: axios로 파싱하기 -> cloudflare cdn에서 제공되는 사이트인 경우 실패

--- back/src/util/parser/parser.js
+++ back/src/util/parser/parser.js
@@ -1,25 +1,69 @@
+import axios from "axios";
 import { parser } from "url-meta-scraper";
 
 async function parsers(url) {
-    const meta = await parser(url);
+    let title = null;
+    let description = null;
+    let img = null;
 
-    const ogTitle = meta.og?.title || undefined;
-    const ogDescription = meta.og?.description || undefined;
-    const ogImg = meta.og?.image || undefined;
+    const urlMetaScraperResponse = await getDataUsingUrlMetaScraper({ url });
 
-    const metaTitle = meta.meta?.title || undefined;
-    const metaDescription = meta.meta?.description || undefined;
-    const metaImg = meta.twitter?.image || undefined;
+    //title, description 둘 중에 아무것도 없다면 <title> 파싱
+    if (urlMetaScraperResponse) {
+        return { ...urlMetaScraperResponse };
+    }
 
-    let title = ogTitle ?? metaTitle;
-    let description = ogDescription ?? metaDescription;
-    let img = ogImg ?? metaImg;
+    const parseTitleTagResponse = await getDataParseTitleTag({ url });
 
-    title = title ?? null;
-    description = description ?? null;
-    img = img ?? null;
+    if (parseTitleTagResponse) {
+        console.log(parseTitleTagResponse);
+        return { title: parseTitleTagResponse.title, description, img };
+    }
 
     return { title, description, img };
+}
+
+async function getDataUsingUrlMetaScraper({ url }) {
+    let title = null;
+    let description = null;
+    let img = null;
+
+    try {
+        const meta = await parser(url);
+
+        const ogTitle = meta.og?.title || undefined;
+        const ogDescription = meta.og?.description || undefined;
+        const ogImg = meta.og?.image || undefined;
+
+        const metaTitle = meta.meta?.title || undefined;
+        const metaDescription = meta.meta?.description || undefined;
+        const metaImg = meta.twitter?.image || undefined;
+
+        title = ogTitle ?? metaTitle;
+        description = ogDescription ?? metaDescription;
+        img = ogImg ?? metaImg;
+
+        return { title, description, img };
+    } catch (e) {
+        console.log(`parser는 이 사이트 ${url}는 파싱할 수 없습니다`);
+        return null;
+    }
+}
+
+async function getDataParseTitleTag({ url }) {
+    let title = null;
+    try {
+        await axios.get(url).then((res) => {
+            const result = res;
+            console.log(result);
+            title = result.split("<title>")[1].split("</title>")[0];
+        });
+
+        return { title };
+    } catch (e) {
+        console.log(`이 사이트 ${url}는 파싱할 수 없습니다`, e.response.data);
+        return null;
+    }
 }
 
 export { parsers };
