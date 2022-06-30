1175d2b48f03a2a0f6932c4faa7256280e6452b1
Hyejin Eom <hjeom.dev@gmail.com>
2022-07-01 00:44:15 +0900

    fix: 원래대로 url-meta-scraper만 이용해서 파싱

--- back/src/util/parser/parser.js
+++ back/src/util/parser/parser.js
@@ -1,54 +1,25 @@
 import { parser } from "url-meta-scraper";
 
 async function parsers(url) {
-    const urlMetaScraperResponse = getDataUsingUrlMetaScraper({ url });
+    const meta = await parser(url);
 
-    //title, description 둘 중에 아무것도 없다면 <title> 파싱
-    if (urlMetaScraperResponse) {
-        return { ...urlMetaScraperResponse };
-    }
+    const ogTitle = meta.og?.title || undefined;
+    const ogDescription = meta.og?.description || undefined;
+    const ogImg = meta.og?.image || undefined;
 
-    const parseTitleTagResponse = getDataParseTitleTag({ url });
+    const metaTitle = meta.meta?.title || undefined;
+    const metaDescription = meta.meta?.description || undefined;
+    const metaImg = meta.twitter?.image || undefined;
 
-    return { ...parseTitleTagResponse };
-}
+    let title = ogTitle ?? metaTitle;
+    let description = ogDescription ?? metaDescription;
+    let img = ogImg ?? metaImg;
 
-async function getDataUsingUrlMetaScraper({ url }) {
-    let title = null;
-    let description = null;
-    let img = null;
+    title = title ?? null;
+    description = description ?? null;
+    img = img ?? null;
 
-    try {
-        const meta = await parser(url);
-
-        const ogTitle = meta.og?.title || undefined;
-        const ogDescription = meta.og?.description || undefined;
-        const ogImg = meta.og?.image || undefined;
-
-        const metaTitle = meta.meta?.title || undefined;
-        const metaDescription = meta.meta?.description || undefined;
-        const metaImg = meta.twitter?.image || undefined;
-
-        title = ogTitle ?? metaTitle;
-        description = ogDescription ?? metaDescription;
-        img = ogImg ?? metaImg;
-
-        return { title, description, img };
-    } catch (e) {
-        console.log(`이 사이트 ${url}는 파싱할 수 없습니다`);
-        return null;
-    }
-}
-
-async function getDataParseTitleTag({ url }) {
-    try {
-        const res = await fetch("http://example.com/");
-
-        return { title: res.text().split("<title>")[1].split("</title>")[0] };
-    } catch (e) {
-        console.log(`이 사이트 ${url}는 파싱할 수 없습니다`);
-        return null;
-    }
+    return { title, description, img };
 }
 
 export { parsers };
